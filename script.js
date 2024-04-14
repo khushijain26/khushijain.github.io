e82b92f25e1402e48f4fbf914d5f7368f1b5a065
khushijain26 <63397532+khushijain26@users.noreply.github.com>
2024-04-13 22:39:22 -0400

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function toggleMenu() {
 var cursor = document.querySelector(".cursor");
     var cursor2 = document.querySelector(".cursor2");
     document.addEventListener("mousemove",function(e){
-      cursor.style.cssText = cursor2.style.cssText = "left: " + e.clientX + "px; top: " + e.clientY + "px;";
-    });
+      cursor.style.marginLeft = `calc(${e.clientX}px)`; 
+      cursor.style.marginTop = `calc(${e.clientY}px)`;    });
 
    
