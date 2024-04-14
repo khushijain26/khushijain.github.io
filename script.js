c32aff1d741b7f4b8b623023e0cc9e8c815952df
khushijain26 <63397532+khushijain26@users.noreply.github.com>
2024-04-13 22:43:26 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,16 +4,9 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
-
-    var cursor = document.querySelector(".cursor");
+   var cursor = document.querySelector(".cursor");
     var cursor2 = document.querySelector(".cursor2");
-   function mouseMoveEnd() {
-  cursor.style.marginLeft = `calc(${cursorinner.style.left} - ${radiusOfCursor}px)`;
-  cursor.style.marginTop = `calc(${cursorinner.style.top} - ${radiusOfCursor}px)`;
-}
+    document.addEventListener("mousemove",function(e){
+      cursor.style.cssText = cursor2.style.cssText = "left: " + e.clientX + "px; top: " + e.clientY + "px;";
+    });
 
-var x;
-document.addEventListener('mousemove', function() { 
-    if (x) clearTimeout(x); 
-    x = setTimeout(mouseMoveEnd, 10); 
-}, false);
